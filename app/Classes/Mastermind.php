465ee3f2af6653fdadf509ee0f04bc2b522e9c11
chavewain <dionisiochavez@gmail.com>
2021-10-27 08:11:21 -0400

    final clean commit

--- app/Classes/Mastermind.php
+++ app/Classes/Mastermind.php
@@ -8,19 +8,14 @@ class Mastermind
 {
 
     public $code = [];
-    private $colors = [1,2,3,4];
 
     public function __construct($code)
     {
         $this->code = $code;
-
-        // exit($this->code);
-
     }
 
     public function GetHints($code)
     {
-
         return array_diff_assoc($code, $this->code) ?? true;
     }
 }
